46330e5fabf6c38ccf1bd644e532cb306f229ec3
Dana Jackson <jacksonedana@gmail.com>
2023-01-13 13:26:00 -0500

    gh pages

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,15 +1,47 @@
 
 
+import React from 'react'
+import universal, { setHasBabelPlugin } from 'react-universal-component'
+
+setHasBabelPlugin()
+
+const universalOptions = {
+  loading: () => null,
+  error: props => {
+    console.error(props.error);
+    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
+  },
+  ignoreBabelRename: true
+}
+
+const t_0 = universal(import('__react_static_root__/src/pages/404.js'), universalOptions)
+      t_0.template = '__react_static_root__/src/pages/404.js'
+      
+const t_1 = universal(import('__react_static_root__/src/pages/About.js'), universalOptions)
+      t_1.template = '__react_static_root__/src/pages/About.js'
+      
+const t_2 = universal(import('__react_static_root__/src/pages/Login.js'), universalOptions)
+      t_2.template = '__react_static_root__/src/pages/Login.js'
+      
+const t_3 = universal(import('__react_static_root__/src/pages/Manifest.js'), universalOptions)
+      t_3.template = '__react_static_root__/src/pages/Manifest.js'
+      
+const t_4 = universal(import('__react_static_root__/src/pages/Visualize.js'), universalOptions)
+      t_4.template = '__react_static_root__/src/pages/Visualize.js'
+      
+const t_5 = universal(import('__react_static_root__/src/containers/Post'), universalOptions)
+      t_5.template = '__react_static_root__/src/containers/Post'
+      
 
 // Template Map
 export default {
-  '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
-'__react_static_root__/src/pages/About.js': require('__react_static_root__/src/pages/About.js').default,
-'__react_static_root__/src/pages/Login.js': require('__react_static_root__/src/pages/Login.js').default,
-'__react_static_root__/src/pages/Manifest.js': require('__react_static_root__/src/pages/Manifest.js').default,
-'__react_static_root__/src/pages/Visualize.js': require('__react_static_root__/src/pages/Visualize.js').default,
-'__react_static_root__/src/containers/Post': require('__react_static_root__/src/containers/Post').default
+  '__react_static_root__/src/pages/404.js': t_0,
+'__react_static_root__/src/pages/About.js': t_1,
+'__react_static_root__/src/pages/Login.js': t_2,
+'__react_static_root__/src/pages/Manifest.js': t_3,
+'__react_static_root__/src/pages/Visualize.js': t_4,
+'__react_static_root__/src/containers/Post': t_5
 }
-
-export const notFoundTemplate = '__react_static_root__/src/pages/404.js'
+// Not Found Template
+export const notFoundTemplate = "__react_static_root__/src/pages/404.js"
 
